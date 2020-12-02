1341e35198cfec63eb4c28791ea1057ac88086c4
bashendriks <bashendriks10@gmail.com>
2020-12-02 12:39:01 +0100

    Add project files.

---
+++ advent of code/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace advent_of_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Data = new List<int>();
+            List<string> results = new List<string>(); 
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\bashe\Desktop\advent of code\day1.txt");
+            foreach(string line in lines)
+            {
+                Data.Add(int.Parse(line));
+            }
+           
+            foreach(int number in Data)
+            {
+                for (int i = 0; i < Data.Count; i++)
+                {
+                    if(Data.Contains(2020 - (number + Data[i])))
+                        {
+                        results.Add($"{number} {Data[i]} {2020 - (number + Data[i])} {(number)*(Data[i])*(2020 - (number + Data[i])) }");
+                    }
+                }
+            }
+
+            foreach(string line in results)
+            {
+                Console.WriteLine(line);
+            }
+            
+        }
+    }
+}
---
+++ dag2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dag2
+{
+    class Program
+    {
+        // (\d+-\d+ \w): (.*?)+
+        static void Main(string[] args)
+        {
+            List<string> Data = new List<string>();
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\bashe\Desktop\advent of code\day1.txt");
+
+
+            foreach (string line in lines)
+            {
+                Data.Add(line);
+            }
+        }
+    }
+}
